951f2c2bd849d660138f2130a9086f89e9e46850
Kiczek, Ethan <ethan_kiczek@harvard.edu>
2015-08-31 11:43:58 -0400

    Overhauling unit)test.py per Jaime's sugestions.

--- edx-in-canvas/settings/unit_test.py
+++ edx-in-canvas/settings/unit_test.py
@@ -1,32 +1,14 @@
-from .local import *
-
-
-DATABASE_APPS_MAPPING = {
-}
-
-DATABASE_MIGRATION_WHITELIST = ['default']
-
-# Make tests faster by using sqlite3
+from .base import *
+ 
+ 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
-    }
-}
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'NAME': '{{ project_name }}',
     },
 }
-
-"""
-Unit tests should be able to run without talking to external services like
-redis/elasticache.  Let's disable the caching middleware, and use the db
-to store session data.
-"""
-MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
-MIDDLEWARE_CLASSES[MIDDLEWARE_CLASSES.index('cached_auth.Middleware')] = \
-    'django.contrib.auth.middleware.AuthenticationMiddleware'
-MIDDLEWARE_CLASSES = tuple(MIDDLEWARE_CLASSES)
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache'
+    },
+}
